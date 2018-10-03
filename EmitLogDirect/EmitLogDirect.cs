a33c821fd202fbf1744151e20c63db90894494ba
jekaterina.lavruhinaffdsfddfsdfsdfsdfdfwefwef <jekaterina.lavruhina@carfinance247.co.uk>
2018-10-03 14:06:19 +0100

    Direct exchange

---
+++ EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace EmitLogDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName="guest",Password="guest" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "cf247",
+                                        durable: true,
+                                        type: "direct");
+
+                var severity = (args.Length > 0) ? args[0] : "info";
+                var message = (args.Length > 1)
+                              ? string.Join(" ", args.Skip(1).ToArray())
+                              : "Hello Ste!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "cf247",
+                                     routingKey: severity,
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
